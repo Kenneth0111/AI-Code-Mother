b8c72dc3b44e4c535fed84e517894d268ef00999
Kenneth0111 <694818974@qq.com>
2026-04-03 11:44:23 +0800

    2.项目初始化

---
+++ src/main/java/com/example/aicodemother/AiCodeMotherBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.aicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeMotherBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeMotherBackendApplication.class, args);
+    }
+
+}
